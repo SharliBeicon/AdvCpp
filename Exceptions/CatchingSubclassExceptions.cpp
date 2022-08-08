7c09636a57f53da2fd95ebefa8f1a4deeeefb054
Charlie Bacon <carlostocinocubelo@gmail.com>
2022-08-08 12:58:19 +0200

    Estructura carpetas y nuevos ejercicios

---
+++ Exceptions/CatchingSubclassExceptions.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+void goesWrong(){
+    bool error1Detected = false;
+    bool error2Detected = true;
+
+    if(error1Detected){
+        throw bad_alloc();
+    }
+    
+    if(error2Detected){
+        throw exception();
+    }
+}
+
+int main(){
+
+    try {
+        goesWrong();
+    } catch (bad_alloc &e) {
+        cout << "Catching bad alloc: " << e.what() << endl;
+    } catch (exception &e){
+        cout << "Catching exception: " << e.what() << endl;
+    }
+
+
+    return 0;
+}
